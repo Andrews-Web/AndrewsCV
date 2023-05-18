f39450cf19a56f23e359ae0f49421bc3006fecee
Andrews-Web <132385496+Andrews-Web@users.noreply.github.com>
2023-05-18 10:17:58 +0200

    Add files via upload

---
+++ Controllers/Contact.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net;
+using Website_CV.Models;
+
+
+namespace Website_CV.Controllers
+{
+    public class Contact : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(ContactModel model)
+        {
+
+            try {
+                MailMessage mm = new MailMessage();
+                mm.From = new MailAddress("androidowens57@gmail.com");
+                mm.To.Add("andrewkyleowens@gmail.com");
+                mm.Subject = model.Name;
+                mm.Body = "Name: " + model.Name + "<br>Email: " + model.Email + "<br>Number: " + model.Number +"<br>"+ model.Details;
+                mm.IsBodyHtml = true;
+
+                SmtpClient smtpClient = new SmtpClient();
+
+                //Create nerwork credential and you need to give from email address and password
+                NetworkCredential networkCredential = new NetworkCredential("androidowens57@gmail.com", "shpggsfkmszxebus");
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = networkCredential;
+                smtpClient.Port = 587;
+                smtpClient.Host = "smtp.gmail.com";
+                smtpClient.EnableSsl = true;
+                smtpClient.Send(mm);
+
+            }catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message.ToString();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Models/ContactModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Website_CV.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Number { get; set; }
+        public string Details { get; set; }
+    }
+}
